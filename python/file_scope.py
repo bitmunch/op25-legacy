21b494da19782ff43e8f22620df0ece04131d4c7
stevie <stevie@65a5c917-d112-43f1-993d-58c26a4786be>
2008-05-13 04:45:41 +0000

    Merge file_rx and file_scope functionality.
    git-svn-id: http://op25.osmocom.org/svn/trunk@58 65a5c917-d112-43f1-993d-58c26a4786be

--- python/file_scope.py
+++ python/file_scope.py
@@ -1,32 +1,29 @@
 #!/usr/bin/env python
 
+import wx
+import sys
+
 from gnuradio import gr, gru
 from gnuradio import eng_notation
 from gnuradio.eng_option import eng_option
-from gnuradio.wxgui import stdgui, fftsink, waterfallsink, scopesink, form, slider
+from gnuradio.wxgui import stdgui, fftsink, waterfallsink, scopesink #, form
 from optparse import OptionParser
-import wx
-import sys
 
 class app_flow_graph(stdgui.gui_flow_graph):
 
     def __init__(self, frame, panel, vbox, argv):
         stdgui.gui_flow_graph.__init__(self)
 
-        self.frame = frame
-        self.panel = panel
-        
         parser = OptionParser(option_class=eng_option)
         parser.add_option("-d", "--decim", type="int", default=256, help="set decimation")
-        parser.add_option("-i", "--input", default="baseband.dat", help="specify input file")
-        parser.add_option("-f", "--frequency", type="eng_float", default=None, help="set center frequency")
-        parser.add_option("-b", "--bandwidth", type="eng_float", default=12.5e3, help="set bandwidth")
-        parser.add_option("-r", "--repeat", action="store_true", default=False, help="repeat in a loop")
-        parser.add_option("-s", "--scale", type="int", default=1, help="FFT scale factor")
-        parser.add_option("-W", "--waterfall", action="store_true", default=False, help="enable waterfall display")
-        parser.add_option("-R", "--float-scope", action="store_true", default=False, help="enable float oscilloscope")
-        parser.add_option("-C", "--complex-scope", action="store_true", default=False, help="enable complex oscilloscope")
-        parser.add_option("-F", "--fft", action="store_false", default=True, help="enable FFT")
+        parser.add_option("-i", "--input", default="baseband.dat", help="set input file")
+        parser.add_option("-l", "--loop", action="store_true", default=False, help="loop input file")
+        parser.add_option("-b", "--bandwidth", type="eng_float", default=None, help="set bandwidth")
+        parser.add_option("-f", "--frequency", type="eng_float", default=0.0, help="set channel frequency", metavar="Hz")
+        parser.add_option("-r", "--rf-squelch", type="eng_float", default=None, help="RF squelch (in dB)", metavar="dB")
+        parser.add_option("-s", "--channel-decim", type="int", default=1, help="channel decimation")
+        parser.add_option("-o", "--output", type="string", default=None, help="output file")
+ 
         (options, args) = parser.parse_args()
         if len(args) != 0:
             parser.print_help()
@@ -35,46 +32,50 @@ class app_flow_graph(stdgui.gui_flow_graph):
         usrp_rate = 64000000
         sample_rate = usrp_rate // options.decim
 
-        FILE = gr.file_source(gr.sizeof_gr_complex, options.input, options.repeat)
+        FILE = gr.file_source(gr.sizeof_gr_complex, options.input, options.loop)
         last = FILE
 
         THROTTLE = gr.throttle(gr.sizeof_gr_complex, sample_rate);
         self.connect(last, THROTTLE)
         last = THROTTLE
 
-        if options.frequency:
+        self.SCOPE1 = fftsink.fft_sink_c(self, panel, fft_size=512, sample_rate=sample_rate)
+        vbox.Add(self.SCOPE1.win, 10, wx.EXPAND)
+        self.SCOPE1.win.Bind(wx.EVT_LEFT_DOWN, self.scope1_on_left_click)
+        self.connect(last, self.SCOPE1)
+
+        if options.bandwidth:
             coeffs = gr.firdes.low_pass(1.0, sample_rate, options.bandwidth/2, options.bandwidth/2, gr.firdes.WIN_HANN)
-            FILT = gr.freq_xlating_fir_filter_ccf(1, coeffs, -options.frequency, sample_rate)
-            self.connect(last, FILT)
-            last = FILT
+            self.FILT = gr.freq_xlating_fir_filter_ccf(options.channel_decim, coeffs, -options.frequency, sample_rate)
+            self.connect(last, self.FILT)
+            last = self.FILT
+            if options.rf_squelch:
+                RFSQL = gr.pwr_squelch_cc(options.rf_squelch, 1e-3, 0, True)
+                self.connect(last, RFSQL)
+                last = RFSQL
 
-        if options.fft:
-            decim_rate = sample_rate//options.scale;
-            DECIM = gr.keep_one_in_n(gr.sizeof_gr_complex, options.scale)
-            SCOPE = fftsink.fft_sink_c (self, panel, fft_size=512, sample_rate=decim_rate)
-            vbox.Add(SCOPE.win, 10, wx.EXPAND)
-            self.connect(last, DECIM, SCOPE)
-            # wx.lib.evtmgr.eventManager.Register(self.mouse, wx.EVT_LEFT_DOWN, SCOPE.win)
-            # self.Bind(wx.EVT_LEFT_DOWN, self.on_left_click)
-        elif options.waterfall:
-            SCOPE = waterfallsink.waterfall_sink_c (self, panel, fft_size=1024, sample_rate=sample_rate)
-            vbox.Add(SCOPE.win, 10, wx.EXPAND)
-            self.connect(last, SCOPE)
-        elif options.complex_scope:
-            SCOPE = scopesink.scope_sink_c(self, panel, sample_rate=sample_rate) 
-            vbox.Add(SCOPE.win, 10, wx.EXPAND)
-            self.connect(last, SCOPE)
-        if options.float_scope:
-            CONV = gr.complex_to_float()
-            SCOPE = scopesink.scope_sink_f(self, panel, sample_rate=sample_rate)          
-            vbox.Add(SCOPE.win, 10, wx.EXPAND)
-            self.connect(last, CONV, SCOPE)
-        last = SCOPE
+        if options.output:
+            OUT = gr.file_sink(gr.sizeof_gr_complex, options.output)
+            self.connect(last, OUT)
 
-    def on_left_click(self, event):
-#         fRel = (event.GetX() - 48) / 15.2 - 20;
-#         print eng_notation.num_to_str(self.frequency(fRel*1e3))
-         print "here"
+#         SCOPE2 = fftsink.fft_sink_c(self, panel, fft_size=512, sample_rate=sample_rate)
+#         vbox.Add(SCOPE2.win, 11, wx.EXPAND)
+#         self.connect(last, SCOPE2)
+
+#         SCOPE2 = scopesink.scope_sink_c(self, panel, sample_rate=sample_rate) 
+#         vbox.Add(SCOPE2.win, 12, wx.EXPAND)
+#         self.connect(last, SCOPE2)
+
+        CONV = gr.complex_to_float()
+        SCOPE2 = scopesink.scope_sink_f(self, panel, sample_rate=sample_rate)         
+        vbox.Add(SCOPE2.win, 11, wx.EXPAND)
+        self.connect(last, CONV, SCOPE2)
+
+    def scope1_on_left_click(self, event):
+        chanWid = 12.5e3
+        x, y = self.SCOPE1.win.GetXY(event)
+        x = ((x * 1e6) // chanWid) * chanWid
+        self.FILT.set_center_freq(-x)
 
 def main ():
     app = stdgui.stdapp(app_flow_graph, "Signal Scope", nstatus=1)
