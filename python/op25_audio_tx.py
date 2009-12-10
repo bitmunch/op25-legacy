e6f0337fb21c9ece9ec2111cd4dd05dd616ba2ef
max <max@65a5c917-d112-43f1-993d-58c26a4786be>
2009-12-10 01:20:51 +0000

    add -i option to op25_audio_tx
    git-svn-id: http://op25.osmocom.org/svn/trunk@195 65a5c917-d112-43f1-993d-58c26a4786be

--- python/op25_audio_tx.py
+++ python/op25_audio_tx.py
@@ -51,10 +51,15 @@ class app_top_block(gr.top_block):
         input_audio_rate = 8000
         output_audio_rate = 48000
         c4fm_rate = 96000
-        self.audio_input  = audio.source(input_audio_rate,  options.audio_input)
+        if not options.input_files:
+            self.audio_input  = audio.source(input_audio_rate,  options.audio_input)
         self.audio_output = audio.sink  (output_audio_rate, options.audio_output)
 
         for i in range (options.nchannels):
+          if options.input_files:
+            t = gr.file_source(gr.sizeof_char,  "baseband-%d.dat" % i, options.repeat)
+            self.vocoders.append(t)
+          else:
             t = gr.multiply_const_ff(32767 * options.audio_gain)
             self.audio_amps.append(t)
             t = gr.float_to_short()
@@ -66,36 +71,42 @@ class app_top_block(gr.top_block):
                                   0,                    # udp port
                                   False)                # dump raw u vectors
             self.vocoders.append(t)
-            t = op25_c4fm_mod.p25_mod(output_sample_rate=c4fm_rate,
+          t = op25_c4fm_mod.p25_mod(output_sample_rate=c4fm_rate,
                                  log=False,
                                  verbose=True)
-            self.c4fm.append(t)
+          self.c4fm.append(t)
 
-            # FIXME: it would seem as if direct output at 48000 would be the
-            # obvious way to go, but for unknown reasons, it produces hideous
-            # distortion in the output waveform.  For the same unknown
-            # reasons, it's clean if we output at 96000 and then decimate 
-            # back down to 48k...
-            t = blks2.rational_resampler_fff(1, 2) # 96000 -> 48000
-            self.filters.append(t)
-            t = gr.multiply_const_ff(options.output_gain)
-            self.output_gain.append(t)
+          # FIXME: it would seem as if direct output at 48000 would be the
+          # obvious way to go, but for unknown reasons, it produces hideous
+          # distortion in the output waveform.  For the same unknown
+          # reasons, it's clean if we output at 96000 and then decimate 
+          # back down to 48k...
+          t = blks2.rational_resampler_fff(1, 2) # 96000 -> 48000
+          self.filters.append(t)
+          t = gr.multiply_const_ff(options.output_gain)
+          self.output_gain.append(t)
 
         for i in range (options.nchannels):
-            self.connect(self.audio_amps[i], self.converters[i], self.vocoders[i], self.c4fm[i], self.filters[i], self.output_gain[i])
+          if not options.input_files:
+            self.connect(self.audio_amps[i], self.converters[i], self.vocoders[i])
+          self.connect(self.vocoders[i], self.c4fm[i], self.filters[i], self.output_gain[i])
         if options.nchannels == 1:
+          if not options.input_files:
             self.connect(self.audio_input, self.audio_amps[0])
-            self.connect(self.output_gain[0], self.audio_output)
+          self.connect(self.output_gain[0], self.audio_output)
         else:
             for i in range (options.nchannels):
+              if not options.input_files:
                 self.connect((self.audio_input, i), self.audio_amps[i])
-                self.connect(self.output_gain[i], (self.audio_output, i))
+              self.connect(self.output_gain[i], (self.audio_output, i))
 
 def main():
     parser = OptionParser(option_class=eng_option)
     parser.add_option("-g", "--audio-gain", type="eng_float", default=1.0, help="input gain factor")
     parser.add_option("-G", "--output-gain", type="eng_float", default=0.6, help="output gain factor")
+    parser.add_option("-i", "--input-files", action="store_true", default=False, help="take input from file(s)")
     parser.add_option("-n", "--nchannels", type="int", default=2, help="number of audio channels")
+    parser.add_option("-r", "--repeat", action="store_true", default=False, help="replay input")
     parser.add_option("-v", "--verbose", action="store_true", default=False, help="dump demodulation data")
     parser.add_option("-I", "--audio-input", type="string", default="", help="pcm input device name.  E.g., hw:0,0 or /dev/dsp")
     parser.add_option("-O", "--audio-output", type="string", default="", help="pcm output device name.  E.g., hw:0,0 or /dev/dsp")
