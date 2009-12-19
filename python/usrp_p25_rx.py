0cb3b759c2326b96e0fb0b9f915c8090cb333d22
stevie <stevie@65a5c917-d112-43f1-993d-58c26a4786be>
2009-12-19 09:53:37 +0000

    Fix transient flag.
    git-svn-id: http://op25.osmocom.org/svn/trunk@198 65a5c917-d112-43f1-993d-58c26a4786be

--- python/usrp_p25_rx.py
+++ python/usrp_p25_rx.py
@@ -96,7 +96,7 @@ class p25_rx_block (stdgui2.std_top_block):
             self.open_file(options.input)
         elif options.frequency:
             self._set_state("CAPTURING")
-            self.open_usrp(options.rx_subdev_spec, options.decim, options.gain, options.frequency, options.transient)
+            self.open_usrp(options.rx_subdev_spec, options.decim, options.gain, options.frequency, not options.transient)
         else:
             self._set_state("STOPPED")
 
